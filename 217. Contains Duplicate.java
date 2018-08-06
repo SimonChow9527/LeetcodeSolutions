ac8d9ac747a0a8d123223635af0154388a4f5d8a
Simon Chow <Melanmare@users.noreply.github.com>
2018-08-07 09:03:50 +1000

    Create 217. Contains Duplicate.java
    Maybe merge sort will be faster than Array.sort.

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        boolean ans=false;
+        for(int i=0;i<nums.length-1;i++)
+            if(nums[i]==nums[i+1])
+                ans=true;
+        
+        return ans;
+    }
+}
